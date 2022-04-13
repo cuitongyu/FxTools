27c10640b97036a93a73699906ae634cfce53f7d
tongyu <476871232@qq.com>
2022-04-13 14:59:19 +0800

    test

--- src/main/java/com/fx/tool/ToolApplication.java
+++ src/main/java/com/fx/tool/ToolApplication.java
@@ -12,6 +12,7 @@ public class ToolApplication extends Application {
     public static void main(String[] args) {
         SpringApplication.run(ToolApplication.class, args);
         launch(args);
+        System.out.println("ce");
     }
 
     /**
